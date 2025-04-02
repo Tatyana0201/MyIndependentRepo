7273212902fb5eb3c6c1daf0179c42b7dc46b5d6
Tatyana0201 <a9110900@gmail.com>
2025-04-02 10:13:21 -0700

    with 6 updates

--- features/environment.py
+++ features/environment.py
@@ -1,15 +1,20 @@
+import json
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+import json
 
-
-# features/steps/steps.py
+def before_all(context):
+    with open("setup.json", "r") as file:
+        context.credentials = json.load(file)
 
 def before_scenario(context, scenario):
     """Setup WebDriver before each scenario"""
     options = Options()
     context.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+
 
 def after_scenario(context, scenario):
     """Quit WebDriver after each scenario"""
--- features/steps/steps.py
+++ features/steps/steps.py
@@ -5,8 +5,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium .webdriver.common.keys import Keys
 from time import sleep
 
-
-
 @step('Open "{env}"environment')
 def open_url(context, env):
     environments = {
@@ -15,32 +13,28 @@ def open_url(context, env):
     }
     context.driver.get(environments[env])
 
-
 @step('T.A. Input following credentials')
 def input_following_credentials(context):
     print("This is my step")
 
 @step('T.A. Type "{text}"into "{xpath}"')
-def type_text(context, text,xpath):
+def type_text(context, text, xpath):
     print("This is my step")
-
 
 # def wait_for_element():
 #     return None
 
-@step ('Click element "{xpath}"')
+@step('Click element "{xpath}"')
 def click_element(context, xpath):
-    element = context.driver.find_element(By.XPATH,xpath)
+    element = context.driver.find_element(By.XPATH, xpath)
 
-
-@step ('Click button "{name}"')
+@step('Click button "{name}"')
 def click_button(context, name):
     buttons = {
         'Login': "//button[text()=' Login ']",
     }
-    element = context.driver.find_element(By.XPATH.buttons[name])
+    element = context.driver.find_element(By.XPATH, buttons[name])
     element.click()
     element.send_keys(Keys.CONTROL + "a")
     element.send_keys(Keys.DELETE)
-
 
